faa38d5fd4fd0d5b520174d6365721dab40d501b
EndermanbugZJFC <175898536+Ezjfc@users.noreply.github.com>
2025-02-06 03:33:43 +0000

    Impl: declaration arg part. Doc: finalised docs in parser.rs

--- src/parse.rs
+++ src/parse.rs
@@ -1,47 +1,95 @@
 //! Parsing the arguments using `clap_lex` to get the correct
-//! offset and length of an argument in the `Argv` string.
-//! For the differences between Argv indices and Clap indicies, please refer to
+//! offset and length of an argument in the Argv string.
+//! For the differences between Argv indices and Clap indicies,
+//! please refer to
 //! https://docs.rs/clap/latest/clap/struct.ArgMatches.html#examples-15
 
-use std::{borrow::BorrowMut, cell::{OnceCell, RefCell}, collections::BTreeMap, ffi::{OsStr, OsString}, rc::Rc};
+use std::{cell::{OnceCell, RefCell}, convert::AsRef, ffi::{OsStr, OsString}, rc::Rc};
 
 use clap::{builder::ValueParser, CommandFactory};
 
-/// TODO
-pub struct ArgLocator {
-    /// TODO
-    pub get_arg_by_alias: Box<dyn Fn(&ArgAlias) -> Rc<clap::Arg>>,
+/// Parses the Argv string and finds a how a specific argument
+/// appears in the Argv string.
+/// See `Self::from_command_factory()` and `Self::get_location()`.
+pub struct ArgLocator<T: Default, V: AsRef<clap::Arg>> {
+    /// A mapping function that returns a `clap::Arg` by its short or
+    /// long aliases.
+    ///
+    /// `T` is passed in the second argument as a temporary storage
+    /// or cache. Wrapping the `clap::Arg` with a reference counted
+    /// smart pointer (`Rc`) is recommended due to multiple aliases
+    /// may lead to the same argument. See `arg_aliases`.
+    pub get_arg_by_alias: Box<dyn Fn(&Self, &T, &ArgAlias) -> V>,
+    arg_aliases: T,
 }
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
-/// TODO
+// Differentiates if an alias is long or short since long aliases
+// can also be one character, i.e. the length of a short.
 pub enum ArgAlias {
+    /// Longs are led by double hyphens in the Argv string.
+    /// Example: `--long`, `--l`.
+    /// Note: hyphens are not included in this `String`.
     Long(String),
+    /// Shorts are led by single hyphen in the Argv string.
+    ///
+    /// Multiple shorts can stick together.
+    /// Example: `-s`, `-abcds`.
     Short(char),
 }
 
 #[derive(Default, Debug)]
+/// # Example (`ArgLocation::Complete`)
+/// ```md
+///                  --abcdefg=...............
+///                  ^ ^      ^^
+/// declaration.offset |      |content.offset
+///    (length=2)      |      |
+///                    |      delimiter.offset
+///          name.offset         (length=1)
+///           (length=7)
+/// ```
 pub struct ArgPart {
-    /// Offset of the leading character in Argv string.
+    /// Offset of the leading character of a part in the Argv string.
     pub offset: usize,
-    /// TODO: OsStr::len()
+    /// Length of a part in the Argv string. For accurate characters
+    /// count, mapping `std::env::ArgsOs` with `OsStr::to_string_lossy`
+    /// before passing it to `ArgLocator::get_location()` is recommended.
     pub length: usize,
 }
 
 #[derive(Debug)]
-/// Location of parts of argument in Argv string.
+/// Represents how an argument appears as a part in the Argv string.
+/// Every argument has the `declaration` and `name` fields.
+/// The former points to the leading hyphens, i.e. `--` or `-`,
+/// distinguished by `ArgPart.length`; The latter points to the name
+/// which for longs, should be right next to the hypens but for shorts,
+/// it depends.
+///
+/// # Example
+/// ```md
+//                       -abcd
+///                      ^   ^
+///            Declaration   Name
+/// ```
 pub enum ArgLocation {
     /// Argument that has no following value, i.e. it exists as a flag.
-    Discrete(ArgPart),
+    Discrete {
+        declaration: ArgPart,
+        name: ArgPart,
+    },
     /// Short that has a value but is not separated by a delimiter.
     /// `name.length` should be `1` here (reserved for future compatibility).
     Stuck {
+        declaration: ArgPart,
         name: ArgPart,
         content: ArgPart,
     },
     /// Argument that has a value which is separated by either a space
     /// or an equal sign (reserved for future compatibility).
+    /// See `ArgPart` for visualised example.
     Complete {
+        declaration: ArgPart,
         name: ArgPart,
         delimiter: ArgPart,
         content: ArgPart,
@@ -50,9 +98,11 @@ pub enum ArgLocation {
 
 const DELIMITER_LENGTH: usize = 1;
 const SHORT_LENGTH: usize = 1;
+const LONG_DECLARATION_LENGTH: usize = 2;
+const SHORT_DECLARATION_LENGTH: usize = 1;
 
 impl ArgLocation {
-    fn new_complete(name: ArgPart, content_length: usize) -> Self {
+    fn new_complete(declaration: ArgPart, name: ArgPart, content_length: usize) -> Self {
         let delimiter = ArgPart {
             offset: name.offset + name.length,
             length: DELIMITER_LENGTH,
@@ -62,36 +112,48 @@ impl ArgLocation {
             length: content_length,
         };
 
-        Self::Complete { name, delimiter, content }
+        Self::Complete { declaration, name, delimiter, content }
     }
 }
 
-impl ArgLocator {
+impl ArgLocator<OnceCell<Vec<(ArgAlias, Rc<clap::Arg>)>>, Rc<clap::Arg>> {
+    /// Returns `Self` with a lazily initialised aliases mapping to
+    /// arguments mapping which is created from `CommandFactory`. (Or
+    /// any types that derive `clap::Parser`.)
     pub fn from_command_factory<C: CommandFactory>() -> Self {
         Self {
-            get_arg_by_alias: Box::new(|alias| {
-                let aliases: OnceCell<BTreeMap<ArgAlias, Rc<clap::Arg>>> = OnceCell::new();
-                Rc::clone(aliases.get_or_init(|| {
-                    let mut aliases = BTreeMap::new();
+            arg_aliases: OnceCell::new(),
+            get_arg_by_alias: Box::new(|_, arg_aliases, alias| {
+                let cache = arg_aliases.get_or_init(|| {
+                    let mut aliases = vec![];
                     for arg in C::command().get_arguments() {
                         let rc = Rc::new(arg.to_owned());
                         if let Some(all_aliases) = arg.get_all_aliases() {
                             for alias in all_aliases.into_iter().chain(arg.get_long()) {
-                                aliases.insert(ArgAlias::Long(alias.to_string()).into(), Rc::clone(&rc));
+                                aliases.push((ArgAlias::Long(alias.to_string()), Rc::clone(&rc)));
                             }
                         }
                         if let Some(short_aliases) = arg.get_all_short_aliases() {
                             for alias in short_aliases.into_iter().chain(arg.get_short()) {
-                                aliases.insert(ArgAlias::Short(alias).into(), Rc::clone(&rc));
+                                aliases.push((ArgAlias::Short(alias), Rc::clone(&rc)));
                             }
                         }
                     }
+                    aliases.sort_unstable_by(|(a, _), (b, _)| a.cmp(&b)); // For binary search.
+
                     aliases
-                }).get(alias).unwrap())
+                });
+                let index = cache
+                    .binary_search_by(|(k, _)| k.cmp(alias))
+                    .expect("All aliases of arg should be already known");
+
+                Rc::clone(&cache[index].1)
             }),
         }
     }
+}
 
+impl<T: Default, V: AsRef<clap::Arg>> ArgLocator<T, V> {
     /// Returns the location of parts of argument in the given `args`.
     /// Returns `None` if the argument never appears in `args`.
     pub fn get_location<R, A>(&self, args: R, arg: &str) -> Option<ArgLocation>
@@ -102,70 +164,83 @@ impl ArgLocator {
         let raw = clap_lex::RawArgs::new(args);
         let cursor = RefCell::new(raw.cursor());
         let mut name = ArgPart::default();
-        let peek_or_discrete = |name: ArgPart| {
+        let peek_or_discrete = |declaration: ArgPart, name: ArgPart| {
             if let Some(peek) = raw.peek(&cursor.borrow()) {
                 // Marks all following arguments that do not start with `-` or `--` as part
                 // of the content.
                 if peek.to_long().is_none() && peek.to_short().is_none() {
-                    return ArgLocation::new_complete(name, peek.to_value_os().len());
+                    return ArgLocation::new_complete(declaration, name, peek.to_value_os().len());
                 }
             }
-            ArgLocation::Discrete(name)
+            ArgLocation::Discrete { declaration, name }
         };
-        const EXPECT: &str = "All aliases of arg should be already known";
 
-        'parser: while let Some(parsed_arg) = raw.next(&mut cursor.borrow_mut()) {
+        'cursor: while let Some(parsed_arg) = raw.next(&mut cursor.borrow_mut()) {
             if let Some((Ok(long), accompany)) = parsed_arg.to_long() {
-                let found = (self.get_arg_by_alias)(&ArgAlias::Long(long.to_string()).into());
+                let found_generic = (self.get_arg_by_alias)(self, &self.arg_aliases, &ArgAlias::Long(long.to_string()).into());
+                let found = found_generic.as_ref();
                 if found.get_id() != arg {
-                    name.offset += long.len() + DELIMITER_LENGTH;
-                    continue 'parser;
+                    name.offset += LONG_DECLARATION_LENGTH + long.len() + DELIMITER_LENGTH;
+                    continue 'cursor;
                 } else {
                     name.length = long.len();
                 }
+                let declaration = ArgPart {
+                    offset: name.offset - LONG_DECLARATION_LENGTH,
+                    length: LONG_DECLARATION_LENGTH,
+                };
                 let name = name; // Safety: make immutable.
 
                 if let Some(value) = accompany {
-                    return Some(ArgLocation::new_complete(name, value.len()));
+                    return Some(ArgLocation::new_complete(declaration, name, value.len()));
                 }
 
-                return Some(peek_or_discrete(name));
+                return Some(peek_or_discrete(declaration, name));
             } else if let Some(mut shorts) = parsed_arg.to_short() {
                 let flag_possible_values = ValueParser::bool()
                     .possible_values()
                     .and_then(|values| Some(values.collect::<Vec<_>>()))
                     .unwrap_or(vec![]);
 
+                let declaration = ArgPart {
+                    offset: name.offset,
+                    length: SHORT_DECLARATION_LENGTH,
+                };
                 let name = &mut name; // Safety: prevent name from moving in this scope.
+                name.offset += SHORT_DECLARATION_LENGTH;
                 'shorts: while let Some(short_os) = shorts.next_flag() {
                     if let Ok(short) = short_os {
-                        let found = (self.get_arg_by_alias)(&ArgAlias::Short(short));
+                        let found_generic = (self.get_arg_by_alias)(self, &self.arg_aliases, &ArgAlias::Short(short));
+                        let found = found_generic.as_ref();
                         if found.get_possible_values() == flag_possible_values {
                             if found.get_id() != arg {
                                 name.offset += SHORT_LENGTH;
                                 continue 'shorts;
                             }
 
-                            return Some(ArgLocation::Discrete(ArgPart { length: 1, ..*name }));
+                            return Some(ArgLocation::Discrete {
+                                declaration,
+                                name: ArgPart { length: 1, ..*name },
+                            });
                         } else {
                             let remain = shorts.next_value_os();
                             if found.get_id() != arg {
                                 name.offset += SHORT_LENGTH
                                     + remain.unwrap_or(OsStr::new("")).len()
                                     + DELIMITER_LENGTH;
-                                continue 'parser;
+                                continue 'cursor;
                             }
 
                             if let Some(stuck) = remain {
                                 let accompany_bytes = stuck.as_encoded_bytes();
                                 if let &[b'=', ..] = accompany_bytes {
-                                    return Some(ArgLocation::new_complete(ArgPart {
+                                    return Some(ArgLocation::new_complete(declaration, ArgPart {
                                         length: SHORT_LENGTH,
                                         ..*name
                                     }, stuck.len() - 1));
                                 }
 
-                                return Some(ArgLocation::Stuck { name: ArgPart {
+                                return Some(ArgLocation::Stuck { declaration, name: ArgPart {
                                     length: SHORT_LENGTH,
                                     ..*name
                                 }, content: ArgPart {
@@ -174,7 +249,7 @@ impl ArgLocator {
                                 }});
                             }
 
-                            return Some(peek_or_discrete(ArgPart {
+                            return Some(peek_or_discrete(declaration, ArgPart {
                                 length: SHORT_LENGTH,
                                 ..*name
                             }));
@@ -183,7 +258,10 @@ impl ArgLocator {
                 }
             } else {
                 name.offset += parsed_arg.to_value_os().len() + DELIMITER_LENGTH;
+                continue 'cursor;
             }
+
+            panic!("All branches should return or continue");
         }
 
         None
